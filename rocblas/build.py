0d564f3aa8a6b62526659fcdbc17b73f6f19c472
Wieceslaw <slava.astra@yahoo.com>
2022-04-13 14:16:43 +0200

    change build logic

--- rocblas/build.py
+++ rocblas/build.py
@@ -1,13 +1,44 @@
 import os
 from argparse import ArgumentParser
-from subprocess import check_output
+from subprocess import check_output, check_call
 import shutil
 
 
 def copy(args):
-    shutil.copytree('/home/georgiy/Desktop/Viacheslav/rocblas/rocBLAS/build/release/rocblas-install', os.environ['PREFIX'], symlinks=True, dirs_exist_ok=True)
+    shutil.copytree(f'/opt/rocm-{args.rocmrelease}/', os.environ['PREFIX'], symlinks=True, dirs_exist_ok=True)
 
+def install_rocblas(args):
+    cmd = [
+            "apt",
+            "download",
+            "rocblas{}".format(args.rocmrelease),
+        ]
+    check_call(cmd)
+    cmd = [
+            "sudo"
+            "dpkg",
+            "-i",
+            "--force-depends"
+            "rocblas5.1.0_2.43.0.50100-36_amd64.deb"
+        ]
+    output = check_output(cmd)
+    with open('rocblas_install.log', 'w') as f:
+        f.write(output.decode())
+        
 
+def uninstall_rocblas(args):
+    cmd = [
+            "sudo",
+            "apt",
+            "remove",
+            "-y",
+            "rocblas{}".format(args.rocmrelease),
+        ]
+    output = check_output(cmd)
+    with open('rocblas_uninstall.log', 'w') as f:
+        f.write(output.decode())
+        
+        
 def make_parser():
     p = ArgumentParser("build.py")
     p.add_argument("--rocmrelease", required=True)
@@ -18,7 +49,9 @@ def main():
     print('Running build')
     parser = make_parser()
     args = parser.parse_args()
-    copy(args)
+    install_rocblas(args)
+    # copy(args)
+    uninstall_rocblas(args)
 
 
 if __name__ == "__main__":
