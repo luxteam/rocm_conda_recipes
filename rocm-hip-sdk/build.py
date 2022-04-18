91543a1dad128fe30565f910c654f6630cc22295
Wieceslaw <slava.astra@yahoo.com>
2022-04-18 21:31:22 +0200

    Add rocblas duct tape

--- rocm-hip-sdk/build.py
+++ rocm-hip-sdk/build.py
@@ -1,11 +1,59 @@
 import os
 from argparse import ArgumentParser
-from subprocess import check_output
+from subprocess import check_output, check_call
 import shutil
+
+rocblas_files_to_delete = [
+    'Kernels.so-000-gfx1010.hsaco',
+    'Kernels.so-000-gfx1011.hsaco',
+    'Kernels.so-000-gfx1012.hsaco',
+    'Kernels.so-000-gfx1030.hsaco',
+    'Kernels.so-000-gfx803.hsaco',
+    'Kernels.so-000-gfx900.hsaco',
+    'Kernels.so-000-gfx906-xnack-.hsaco',
+    'Kernels.so-000-gfx908-xnack-.hsaco',
+    'Kernels.so-000-gfx90a-xnack+.hsaco',
+    'Kernels.so-000-gfx90a-xnack-.hsaco',
+    'TensileLibrary.dat',
+    'TensileLibrary_gfx1030.co',
+    'TensileLibrary_gfx803.co',
+    'TensileLibrary_gfx900.co',
+    'TensileLibrary_gfx906.co',
+    'TensileLibrary_gfx908.co',
+    'TensileLibrary_gfx90a.co',
+]
+
+
+def archive_rocblas_binaries(args):
+    cmd = [
+        "sudo",
+        "zip",
+        "-rj",
+        "/opt/rocm-{}/rocblas/lib/library/library.zip".format(args.rocmrelease),
+        "/opt/rocm-{}/rocblas/lib/library".format(args.rocmrelease),
+    ]
+    check_call(cmd)
+    for ftd in rocblas_files_to_delete:
+        cmd = [
+            "sudo",
+            "rm",
+            "/opt/rocm-{}/rocblas/lib/library/{}".format(args.rocmrelease, ftd),
+        ]
+        check_call(cmd)
+
+
+def remove_zip(args):
+    cmd = [
+            "sudo",
+            "rm",
+            "/opt/rocm-{}/rocblas/lib/library/library.zip".format(args.rocmrelease)
+        ]
+    check_call(cmd)
 
 
 def copy(args):
     shutil.copytree(f'/opt/rocm-{args.rocmrelease}/', os.environ['PREFIX'], symlinks=True, dirs_exist_ok=True)
+
 
 def install_rocm(args):
     cmd = [
@@ -44,7 +92,9 @@ def main():
     parser = make_parser()
     args = parser.parse_args()
     install_rocm(args)
+    archive_rocblas_binaries(args)
     copy(args)
+    remove_zip(args)
     uninstall_rocm(args)
 
 
