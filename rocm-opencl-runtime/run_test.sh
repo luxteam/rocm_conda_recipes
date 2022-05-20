84216e57cfb0f9c577c44e897929f30831b33898
Wieceslaw <slava.astra@yahoo.com>
2022-05-20 10:11:47 +0200

    Remove tests for opencl-runtime package

--- rocm-opencl-runtime/run_test.sh
+++
@@ -1,4 +0,0 @@
-cd test
-bash run-opencl.sh
-bash run-opencl2.sh
-bash run-opencl3.sh
--- rocm-opencl-runtime/test/run-opencl3.sh
+++
@@ -1,7 +0,0 @@
-
-mkdir -p build
-
-g++ -std=c++17 -o build/test_opencl3 src/test_opencl3.cpp src/Timer.cpp -g -lOpenCL -L$CONDA_PREFIX/opencl/lib -I$CONDA_PREFIX/opencl/include -O3
-
-./build/test_opencl3
-
