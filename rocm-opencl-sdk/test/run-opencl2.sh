418f1520284988d1059cfaaae8960ebc0f7a0eaf
Wieceslaw <slava.astra@yahoo.com>
2022-05-24 10:11:34 +0200

    Change lib path in opencl tests

--- rocm-opencl-sdk/test/run-opencl2.sh
+++ rocm-opencl-sdk/test/run-opencl2.sh
@@ -1,7 +1,7 @@
 
 mkdir -p build
 
-g++ -std=c++17 -o build/test_opencl2 src/test_opencl2.cpp -g -lOpenCL -L$CONDA_PREFIX/opencl/lib -I$CONDA_PREFIX/opencl/include -O3
+g++ -std=c++17 -o build/test_opencl2 src/test_opencl2.cpp -g -lOpenCL -L$CONDA_PREFIX/lib -I$CONDA_PREFIX/opencl/include -O3
 
 ./build/test_opencl2
 
