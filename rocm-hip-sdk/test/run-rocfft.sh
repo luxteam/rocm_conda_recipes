e7f7e61b17668814e56f23710b9aeafe9e73583d
Wieceslaw <slava.astra@yahoo.com>
2022-05-30 18:15:33 +0200

    Replace TEST_PREFIX with CONDA_PREFIX

--- rocm-hip-sdk/test/run-rocfft.sh
+++ rocm-hip-sdk/test/run-rocfft.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 
-hipcc -D__HIP_PLATFORM_HCC__ -lrocfft -lhipfft -L$TEST_PREFIX/lib src/test_rocfft.cpp -o build/test_rocfft
+hipcc -D__HIP_PLATFORM_HCC__ -lrocfft -lhipfft -L$CONDA_PREFIX/lib src/test_rocfft.cpp -o build/test_rocfft
 
 ROCBLAS_LAYER=0xf TENSILE_DB=0xffff ./build/test_rocfft
 
--- rocm-ml-libraries/test/run-rocblas.sh
+++ rocm-ml-libraries/test/run-rocblas.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 
-hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L$TEST_PREFIX/lib src/test_rocblas.cpp -o build/test_rocblas
+hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L$CONDA_PREFIX/lib src/test_rocblas.cpp -o build/test_rocblas
 
 ROCBLAS_LAYER=0xf TENSILE_DB=0xffff ./build/test_rocblas
 
