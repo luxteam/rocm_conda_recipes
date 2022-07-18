2e86a7939e89cd6687594032690be531528d64ea
Wieceslaw <slava.astra@yahoo.com>
2022-07-18 04:54:28 +0200

    Add cpu type

--- pytorch/build.sh
+++ pytorch/build.sh
@@ -2,18 +2,22 @@
 set -ex
 
 export CMAKE_PREFIX_PATH=$CONDA_PREFIX
-export HIP_PATH=$CONDA_PREFIX/hip
-export ROCM_PATH=$CONDA_PREFIX
-export ROCM_SOURCE_DIR=$CONDA_PREFIX
 export USE_NINJA=0
-export USE_ROCM=1
-export USE_MAGMA=1
+if [[ ${rocm_compiler} != "None" ]]; then
+    export HIP_PATH=$CONDA_PREFIX/hip
+    export ROCM_PATH=$CONDA_PREFIX
+    export ROCM_SOURCE_DIR=$CONDA_PREFIX
+    export USE_ROCM=1
+    export USE_MAGMA=1
+    which rocminfo
+    rocminfo
+fi
 
-which rocminfo
-rocminfo
 which gcc
 gcc --version
 
 rm -rf build
-python3 tools/amd_build/build_amd.py
+if [[ ${rocm_compiler} != "None" ]]; then
+    python3 tools/amd_build/build_amd.py
+fi
 python3 setup.py install
